1c6509358a2249f5b1db156ac0da04309f91cce5
Sarah Vessels <cheshire137@gmail.com>
2024-07-04 13:38:55 -0500

    Show color count

--- color-conversion-table/src/Header.tsx
+++ color-conversion-table/src/Header.tsx
@@ -1,7 +1,19 @@
-import {Header as PrimerHeader, Heading} from '@primer/react'
+import {CounterLabel, Header as PrimerHeader, Heading} from '@primer/react'
+import {useColors} from './ColorsContext'
 
-export const Header = () => (
-  <PrimerHeader className="noprint">
-    <Heading as="h1">Embroidery floss color conversion</Heading>
-  </PrimerHeader>
-)
+export const Header = () => {
+  const {colors} = useColors()
+  const units = colors.length === 1 ? 'color' : 'colors'
+  return (
+    <PrimerHeader className="noprint">
+      <PrimerHeader.Item>
+        <Heading as="h1">
+          Embroidery floss color conversion
+        </Heading>
+      </PrimerHeader.Item>
+      <PrimerHeader.Item>
+        <CounterLabel scheme="primary" sx={{fontSize: 1, p: 2}}>{colors.length} {units}</CounterLabel>
+      </PrimerHeader.Item>
+    </PrimerHeader>
+  )
+}
