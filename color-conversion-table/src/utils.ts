93770926d070798999ef942a17a751b9362ab652
Sarah Vessels <cheshire137@gmail.com>
2024-07-04 12:46:45 -0500

    Add chunkArray util

--- color-conversion-table/src/utils.ts
+++ color-conversion-table/src/utils.ts
@@ -1,3 +1,12 @@
 export const normalizeDmcCode = (dmcCode: string) => {
   return dmcCode.trim().toLowerCase()
 }
+
+export const chunkArray = <T>(array: T[], size: number): T[][] => {
+  const chunks: T[][] = []
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = array.slice(i, i + size);
+    chunks.push(chunk)
+  }
+  return chunks
+}
