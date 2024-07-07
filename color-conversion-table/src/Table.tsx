a0562027508d278cb102ce1ab2c50bd8e9557cfb
Sarah Vessels <cheshire137@gmail.com>
2024-07-07 15:39:37 -0500

    Improve table styling

--- color-conversion-table/src/Table.tsx
+++ color-conversion-table/src/Table.tsx
@@ -1,5 +1,6 @@
-import {Box} from '@primer/react'
-import {TableRow} from './TableRow'
+import deepmerge from 'deepmerge'
+import {Box, type SxProp} from '@primer/react'
+import {TableRow, codeCellStyles, jpCoatsCellStyles} from './TableRow'
 import type {EmbroideryFlossColor} from './types'
 import {useHideColors} from './HideColorsContext'
 import './Table.css'
@@ -11,6 +12,31 @@ interface TableProps {
 
 export const Table = ({colors}: TableProps) => {
   const {hideColors} = useHideColors()
+  const jpCoatsHeaderStyles = deepmerge<SxProp['sx']>(
+    {
+      borderLeft: '1px solid',
+      borderRight: '1px solid',
+      backgroundColor: primerPrimitives.colors.light.canvas.subtle,
+    },
+    jpCoatsCellStyles
+  )
+  const jpCoatsOldStyles = deepmerge<SxProp['sx']>(
+    {
+      borderLeft: '1px solid',
+      textAlign: 'left',
+      backgroundColor: primerPrimitives.colors.light.canvas.subtle,
+    },
+    jpCoatsCellStyles
+  )
+  const jpCoatsNewStyles = deepmerge<SxProp['sx']>(
+    {
+      borderRight: '1px solid',
+      textAlign: 'left',
+      backgroundColor: primerPrimitives.colors.light.canvas.subtle,
+    },
+    jpCoatsCellStyles
+  )
+
   return (
     <table>
       <thead>
@@ -18,11 +44,7 @@ export const Table = ({colors}: TableProps) => {
           <Box as="th" colSpan={2}>
             DMC
           </Box>
-          <Box
-            as="th"
-            sx={{backgroundColor: primerPrimitives.colors.light.canvas.subtle}}
-            colSpan={2}
-          >
+          <Box as="th" sx={jpCoatsHeaderStyles} colSpan={2}>
             J&amp;P Coats
           </Box>
           <th rowSpan={2}>Anchor</th>
@@ -32,32 +54,25 @@ export const Table = ({colors}: TableProps) => {
           <Box as="th" sx={{textAlign: 'left'}}>
             Name
           </Box>
-          <Box as="th" sx={{textAlign: 'left'}}>
+          <Box as="th" sx={codeCellStyles}>
             Code
           </Box>
-          <Box
-            as="th"
-            sx={{
-              textAlign: 'left',
-              backgroundColor: primerPrimitives.colors.light.canvas.subtle,
-            }}
-          >
+          <Box as="th" sx={jpCoatsOldStyles}>
             Old
           </Box>
-          <Box
-            as="th"
-            sx={{
-              textAlign: 'left',
-              backgroundColor: primerPrimitives.colors.light.canvas.subtle,
-            }}
-          >
+          <Box as="th" sx={jpCoatsNewStyles}>
             New
           </Box>
         </tr>
       </thead>
       <tbody>
-        {colors.map(({dmcCode, ...data}) => (
-          <TableRow key={dmcCode} dmcCode={dmcCode} {...data} />
+        {colors.map(({dmcCode, ...data}, index) => (
+          <TableRow
+            key={dmcCode}
+            dmcCode={dmcCode}
+            isOdd={index % 2 !== 0}
+            {...data}
+          />
         ))}
       </tbody>
     </table>
--- color-conversion-table/src/TableRow.tsx
+++ color-conversion-table/src/TableRow.tsx
@@ -1,26 +1,77 @@
+import deepmerge from 'deepmerge'
+import {Box, type SxProp} from '@primer/react'
 import type {EmbroideryFlossColor} from './types'
 import {useHideColors} from './HideColorsContext'
+import primerPrimitives from '@primer/primitives'
 
-interface TableRowProps extends EmbroideryFlossColor {}
+interface TableRowProps extends EmbroideryFlossColor {
+  isOdd: boolean
+}
+
+export const codeCellStyles: SxProp['sx'] = {textAlign: 'center'}
+
+export const jpCoatsCellStyles = deepmerge<SxProp['sx']>(
+  {
+    borderColor: primerPrimitives.colors.light.border.default,
+  },
+  codeCellStyles
+)
+
+const emptyCellContent = (
+  <Box as="span" sx={{color: primerPrimitives.colors.light.fg.muted}}>
+    &mdash;
+  </Box>
+)
 
 export const TableRow = ({
   dmcCode,
   dmcName,
+  isOdd,
   jpCoatsNew,
   jpCoatsOld,
   anchorCode,
   hexCode,
 }: TableRowProps) => {
   const {hideColors} = useHideColors()
+  const jpCoatsBg = isOdd
+    ? primerPrimitives.colors.light.neutral.muted
+    : primerPrimitives.colors.light.canvas.subtle
+  const jpCoatsOldStyles = deepmerge<SxProp['sx']>(
+    {
+      borderLeft: '1px solid',
+      backgroundColor: jpCoatsBg,
+    },
+    jpCoatsCellStyles
+  )
+  const jpCoatsNewStyles = deepmerge<SxProp['sx']>(
+    {
+      borderRight: '1px solid',
+      backgroundColor: jpCoatsBg,
+    },
+    jpCoatsCellStyles
+  )
+  const rowStyles: SxProp['sx'] = {
+    borderTop: '1px solid',
+    borderColor: primerPrimitives.colors.light.border.default,
+    '&:nth-of-type(even)': {
+      backgroundColor: primerPrimitives.colors.light.canvas.subtle,
+    },
+  }
 
   return (
-    <tr>
+    <Box as="tr" sx={rowStyles}>
       <td>{dmcName}</td>
-      <td>{dmcCode}</td>
-      <td>{jpCoatsOld ? jpCoatsOld : <>&mdash;</>}</td>
-      <td>{jpCoatsNew ? jpCoatsNew : <>&mdash;</>}</td>
-      <td>{anchorCode ? anchorCode : <>&mdash;</>}</td>
+      <Box as="td" sx={codeCellStyles}>
+        {dmcCode}
+      </Box>
+      <Box as="td" sx={jpCoatsOldStyles}>
+        {jpCoatsOld ? jpCoatsOld : emptyCellContent}
+      </Box>
+      <Box as="td" sx={jpCoatsNewStyles}>
+        {jpCoatsNew ? jpCoatsNew : emptyCellContent}
+      </Box>
+      <Box as="td" sx={codeCellStyles}>{anchorCode ? anchorCode : emptyCellContent}</Box>
       {!hideColors && <td style={{backgroundColor: hexCode}}></td>}
-    </tr>
+    </Box>
   )
 }
